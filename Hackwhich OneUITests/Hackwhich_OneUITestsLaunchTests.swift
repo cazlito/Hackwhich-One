c2164ee98d21567ecb52b999e0f0ce65401bc659
Cazandara Rufo <cmrufo16@icloud.com>
2023-08-22 13:40:56 -1000

    Initial Commit

---
+++ Hackwhich OneUITests/Hackwhich_OneUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Hackwhich_OneUITestsLaunchTests.swift
+//  Hackwhich OneUITests
+//
+//  Created by Cazandra Rufo on 8/22/23.
+//
+
+import XCTest
+
+final class Hackwhich_OneUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
